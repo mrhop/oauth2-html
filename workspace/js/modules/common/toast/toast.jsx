c2d96fca81d03d201c7afef34a594ede5d4f0ba3
mrhop <233402923@qq.com>
2016-06-02 17:30:01 +0800

    it is the begin

--- workspace/js/modules/common/toast/toast.jsx
+++ workspace/js/modules/common/toast/toast.jsx
@@ -3,17 +3,12 @@
  */
 require('./toast.scss');
 function createToast(toastValues, type) {
-    if (!document.querySelector('#toast-container')) {
-        var elem = document.createElement('div');
-        elem.id = 'toast-container';
-        elem.classList.add('right-top');
-        document.body.insertBefore(elem, document.body.childNodes[0]);
-    }
+    //NEED TO COMPONENT
+    let toastWrapper = ReactDOM.render(<ToastWrapper />, document.body.Element);
+
+    toastWrapper.addToast()
     if (!type || type == 'default') {
-        ReactDOM.render(
-            <DefaultToast toastValues={toastValues}/>,
-            document.querySelector('#toast-container')
-        );
+        toastWrapper.addToast( <DefaultToast toastValues={toastValues}/>);
     } else {
         //other creation
     }
@@ -27,13 +22,16 @@ class ToastWrapper extends React.Component {
         this.state = {data: []};
     }
 
+    addToast(toast){
+        this.state.data.push(toast);
+    }
     render() {
         var toasts = this.state.data.map(function (toast) {
             return (
             {toast}
             );
         });
-        return (<div id="toast-container" className="right-top">{toast}</div> );
+        return (<div id="toast-container" className="right-top">{toasts}</div> );
     }
 }
 
